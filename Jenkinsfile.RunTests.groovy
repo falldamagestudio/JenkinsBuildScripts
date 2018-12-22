ebdec2e259f0a670bc1d0990fd11cb2f60e9d05d
kalms@falldamagestudio.com <>
2018-12-22 18:27:29 +0100

    just run gradle, without try-catching

--- Jenkinsfile.RunTests.groovy
+++ Jenkinsfile.RunTests.groovy
@@ -6,21 +6,22 @@ pipeline {
 	stages {
 		stage('Run Tests') {
 			steps {
-				script {
-					try {
-						if (isWindows()) {
-							bat 'gradlew test'
-						} else {
-							sh './gradle test'
-						}
-					}
-					catch (err) {
-						currentBuild.result = 'FAILED'
-					}
-					finally {
-						junit testResults: '**/build/test-reports/*.xml'
-					}
-				}
+				sh './gradle test'
+				//script {
+				//	try {
+				//		if (isWindows()) {
+				//			bat 'gradlew test'
+				//		} else {
+				//			sh './gradle test'
+				//		}
+				//	}
+				//	catch (err) {
+				//		currentBuild.result = 'FAILED'
+				//	}
+				//	finally {
+				//		junit testResults: '**/build/test-reports/*.xml'
+				//	}
+				//}
 			}
 		}
 	}

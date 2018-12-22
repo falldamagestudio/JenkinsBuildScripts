88268cc414514f67e3faaaf062aceeeaac18fb38
kalms@falldamagestudio.com <>
2018-12-22 20:26:12 +0100

    Try messing with healthScaleFactor

--- Jenkinsfile.RunTests.groovy
+++ Jenkinsfile.RunTests.groovy
@@ -8,7 +8,7 @@ pipeline {
 			steps {
 				sh 'chmod u+x gradlew'
 				sh './gradlew test -DignoreFailures=true'
-				junit '**/build/test-results/test/*.xml'
+				junit '**/build/test-results/test/*.xml' healthScaleFactor: 10000
 				//script {
 				//	try {
 				//		if (isWindows()) {

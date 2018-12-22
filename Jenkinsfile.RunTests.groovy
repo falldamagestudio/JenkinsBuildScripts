9dae2b038d0af394137ddd5cd4ae532025b3ba18
kalms@falldamagestudio.com <>
2018-12-22 20:19:18 +0100

    wildcard at start of JUnit path

--- Jenkinsfile.RunTests.groovy
+++ Jenkinsfile.RunTests.groovy
@@ -8,7 +8,7 @@ pipeline {
 			steps {
 				sh 'chmod u+x gradlew'
 				sh './gradlew test -DignoreFailures=true'
-				junit './build/test-results/test/*.xml'
+				junit '**/build/test-results/test/*.xml'
 				//script {
 				//	try {
 				//		if (isWindows()) {

d1fc78bf26c9792e306fe39531c98f7e3d5e1ec2
kalms@falldamagestudio.com <>
2018-12-22 20:30:28 +0100

    health scale factor set to 0. does that make the test suite pass?

--- Jenkinsfile.RunTests.groovy
+++ Jenkinsfile.RunTests.groovy
@@ -8,7 +8,7 @@ pipeline {
 			steps {
 				sh 'chmod u+x gradlew'
 				sh './gradlew test -DignoreFailures=true'
-				junit testResults: '**/build/test-results/test/*.xml', healthScaleFactor: 10000
+				junit testResults: '**/build/test-results/test/*.xml', healthScaleFactor: 0.0
 				//script {
 				//	try {
 				//		if (isWindows()) {

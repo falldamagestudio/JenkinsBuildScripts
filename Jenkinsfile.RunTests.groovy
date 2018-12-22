51166e479cf34faf300e5e6b5e6300294d4f4f02
kalms@falldamagestudio.com <>
2018-12-22 20:36:31 +0100

    Cleanup of jenkinsfile

--- Jenkinsfile.RunTests.groovy
+++ Jenkinsfile.RunTests.groovy
@@ -6,30 +6,21 @@ pipeline {
 	stages {
 		stage('Run Tests') {
 			steps {
+				// Ensure gradlew is executable (Plastic does not mark it as such at checkout)
 				sh 'chmod u+x gradlew'
+
+				// Run tests; ensure that test failures do not abort the build stage
 				sh './gradlew test -DignoreFailures=true'
+
+				// Upload test results to Jenkins
 				junit '**/build/test-results/test/*.xml'
+
+				// Convert 'UNSTABLE' test results to 'FAILED'
 				script {
 					if (currentBuild.result == 'UNSTABLE') {
-						echo "UNSTABLE!"
 						currentBuild.result = 'FAILED'
 					}
 				}
-				//script {
-				//	try {
-				//		if (isWindows()) {
-				//			bat 'gradlew test'
-				//		} else {
-				//			sh './gradle test'
-				//		}
-				//	}
-				//	catch (err) {
-				//		currentBuild.result = 'FAILED'
-				//	}
-				//	finally {
-				//		junit testResults: '**/build/test-reports/*.xml'
-				//	}
-				//}
 			}
 		}
 	}

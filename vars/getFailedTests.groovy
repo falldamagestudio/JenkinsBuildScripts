a3759f8bc55be9ad52f822d631a337654fcfe622
kalms@falldamagestudio.com <>
2018-12-23 15:32:08 +0100

    Try with an import statement

--- vars/getFailedTests.groovy
+++ vars/getFailedTests.groovy
@@ -1,7 +1,7 @@
-
+import com.falldamagestudio.TestResults
 
 def call() {
     echo "getting failed tests"
-    //def testResults = new TestResults().run()
-    //echo "test results: ${testResults}"
+    def testResults = new TestResults().run()
+    echo "test results: ${testResults}"
 }

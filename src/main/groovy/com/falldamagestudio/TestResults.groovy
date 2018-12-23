c755c29eb789ba093cc89f6eb77cee5a81bd6759
kalms@falldamagestudio.com <>
2018-12-23 15:37:12 +0100

    Put src logic into a package

--- src/main/groovy/com/falldamagestudio/TestResults.groovy
+++ src/main/groovy/com/falldamagestudio/TestResults.groovy
@@ -1,3 +1,5 @@
+package com.falldamagestudio
+
 import hudson.tasks.test.AbstractTestResultAction
 
 AbstractTestResultAction testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)

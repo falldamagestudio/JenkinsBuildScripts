4b1ea2d06c8e3f6622a5e89546c1e745c98dfc9e
kalms@falldamagestudio.com <>
2018-12-23 18:34:32 +0100

    type logging within getFailedTests

--- src/com/falldamagestudio/TestResults.groovy
+++ src/com/falldamagestudio/TestResults.groovy
@@ -1,18 +1,37 @@
 package com.falldamagestudio
 
 import hudson.tasks.test.AbstractTestResultAction
+import hudson.tasks.junit.CaseResult
+
+class FailedTest {
+    String name
+}
 
 def getFailedTests() {
 
+    println "currentBuild type: {$currentBuild.getClass().getName()}"
+
     AbstractTestResultAction testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-    echo testResultAction
     if (testResultAction != null) {
-        def failedTests = testResultAction.failedTests
-        echo failedTests
-        return failedTests
+        def failedTests0 = testResultAction.failedTests
+        if (failedTests0 != null)
+            return toFailedTests(failedTests0)
+        else
+            return null
     }
     else
         return null
+}
+
+def toFailedTests(List<CaseResult> failedTests0) {
+    def failedTests = new ArrayList<FailedTest>()
+
+    for (CaseResult failedTest0 : failedTests0) {
+        def failedTest = new FailedTest()
+        failedTest.name = failedTest0.getDisplayName()
+        failedTests.add(failedTest)
+    }
+    return failedTests
 }
 
 return this

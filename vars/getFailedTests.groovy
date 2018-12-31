393414e9fae91a56d292cbab1e1db58893ecbd7a
kalms@falldamagestudio.com <>
2018-12-31 14:21:18 +0100

    Return failed tests at end of getFailedTests(). Test pipeline prints failed tests, this will allow for regression testing later.

--- vars/getFailedTests.groovy
+++ vars/getFailedTests.groovy
@@ -1,12 +1,6 @@
 
 def call() {
-    echo "getFailedTests (var) () starts"
-    echo "currentBuild: ${currentBuild}"
-    echo "getting failed tests"
-    echo "this: ${this}"
     def testResults = new com.falldamagestudio.TestResults(this)
-    echo "Created test object"
-    echo "test results: ${testResults}"
     def failedTests = testResults.getFailedTests()
-    echo "failed tests: ${failedTests}"
+    return failedTests
 }

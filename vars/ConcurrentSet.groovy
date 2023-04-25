9d6511311c5c670d58ab5b66ff5c0a4c351c62c4
Anton Suchov <asuchov42@gmail.com>
2023-04-25 10:38:46 +0200

    Added ConcurrentSet class

---
+++ vars/ConcurrentSet.groovy
@@ -0,0 +1,31 @@
+import groovy.transform.CompileStatic
+import java.util.concurrent.ConcurrentHashMap
+
+@CompileStatic
+class ConcurrentSet {
+
+    Set<String> set = Collections.newSetFromMap(new ConcurrentHashMap<String, Boolean>());
+
+    void add(String stageName) {
+        set.add(stageName);
+    }
+
+    void remove(String stageName) {
+        set.remove(stageName);
+    }
+
+    /**
+    * Returns true if set is empty
+    */
+    boolean isEmpty() {
+        return set.empty;
+    }
+
+    /**
+    * returns list of keys
+    */
+    List<String> getKeys() {
+        return set.toList();
+    }
+
+}

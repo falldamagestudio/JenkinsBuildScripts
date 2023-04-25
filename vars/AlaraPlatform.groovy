f8e2588db49c3aae3a31be7cbaa5c226b62837cc
Anton Suchov <asuchov42@gmail.com>
2023-04-25 13:49:59 +0200

    AlaraPlatform class added

---
+++ vars/AlaraPlatform.groovy
@@ -0,0 +1,29 @@
+import groovy.transform.CompileStatic
+
+@CompileStatic
+enum AlaraPlatformEnum {
+
+    WINDOWS,
+    LINUX,
+
+}
+
+@CompileStatic
+class AlaraPlatform {
+
+    @Lazy static AlaraPlatformEnum currentPlatform = {
+        String os = System.getProperty('os.name');
+        if (os.contains('Windows')) {
+            return AlaraPlatformEnum.WINDOWS;
+        } else if (os.contains('Linux')) {
+            return AlaraPlatformEnum.LINUX;
+        }
+
+        throw new UnsupportedOperationException('Unsupported OS: ' + os);
+     } ();
+
+    @Lazy static boolean isWindows = { currentPlatform == AlaraPlatformEnum.WINDOWS } ();
+
+    @Lazy static boolean isLinux = { currentPlatform == AlaraPlatformEnum.LINUX } ();
+
+}

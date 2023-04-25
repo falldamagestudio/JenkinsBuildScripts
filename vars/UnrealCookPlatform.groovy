41ada2e609dc0f20d24492fbfab2179c73d740da
Anton Suchov <asuchov42@gmail.com>
2023-04-25 09:42:31 +0200

    Added UnrealCookPlatform

---
+++ vars/UnrealCookPlatform.groovy
@@ -0,0 +1,26 @@
+import groovy.transform.CompileStatic
+
+/**
+ * The {@code UnrealPlatform} class represents the platforms supported by Unreal Engine.
+ * <p>
+ * This class is annotated with {@code @CompileStatic} to generate statically-typed
+ * bytecode for better performance and reduced memory usage at runtime.
+ * <p>
+ * Constants below correspond to Unreal Automation tool cook platform constants
+ * They are hardcoded in GetCookPlatform function
+ */
+
+@CompileStatic
+class UnrealCookPlatform {
+
+    // in UE4\Engine\Source\Programs\AutomationTool\Win\WinPlatform.Automation.cs
+    static final String COOK_PLATFORM_WINDOWS = 'WindowsNoEditor';
+    static final String COOK_PLATFORM_WINDOWS_SERVER = 'WindowsServer';
+    static final String COOK_PLATFORM_WINDOWS_CLIENT = 'WindowsClient';
+
+    // in UE4\Engine\Source\Programs\AutomationTool\Linux\LinuxPlatform.Automation.cs
+    static final String COOK_PLATFORM_LINUX = 'LinuxNoEditor';
+    static final String COOK_PLATFORM_LINUX_SERVER = 'LinuxServer';
+    static final String COOK_PLATFORM_LINUX_CLIENT = 'LinuxClient';
+
+}

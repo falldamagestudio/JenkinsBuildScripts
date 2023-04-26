f60d662439bd8da336fd2473a2fc3c4689b630d3
Anton Suchov <asuchov42@gmail.com>
2023-04-26 16:14:56 +0200

    Fixed fucntion arguments

--- vars/alaraRun.groovy
+++ vars/alaraRun.groovy
@@ -1,6 +1,6 @@
 import com.falldamagestudio.Alara;
 
-void call(String cmd, List arguments) {
+void call(String cmd, Object[] arguments) {
     Alara alara = new Alara(this);
     alara.run(cmd, arguments);
 }

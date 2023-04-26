5aad55ecdc959f57e0bd6b49a1149bca8b2a6285
Anton Suchov <asuchov42@gmail.com>
2023-04-26 19:37:44 +0200

    Introducing AutoMagic call

---
+++ src/com/falldamagestudio/AutoMagic.groovy
@@ -0,0 +1,9 @@
+package com.falldamagestudio
+
+class AutoMagic {
+
+    static void run(Object script, String cmdString) {
+        Alara.run(script, cmdString);
+    }
+
+}
---
+++ vars/autoMagicRun.groovy
@@ -0,0 +1,5 @@
+import com.falldamagestudio.AutoMagic
+
+void call(String cmdString) {
+    AutoMagic.run(this, cmdString);
+}

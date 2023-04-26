9badf535e37329397f699c6058c05c3502120518
Anton Suchov <asuchov42@gmail.com>
2023-04-26 16:18:00 +0200

    Testing function overload

--- vars/alaraRun.groovy
+++ vars/alaraRun.groovy
@@ -1,5 +1,10 @@
 import com.falldamagestudio.Alara;
 
+void call(String cmd) {
+    Alara alara = new Alara(this);
+    alara.run(cmd);
+}
+
 void call(String cmd, Object[] arguments) {
     Alara alara = new Alara(this);
     alara.run(cmd, arguments);

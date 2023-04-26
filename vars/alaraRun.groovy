33c88ed11ccfcac8a805ec8866f4ed7664ddea4c
Anton Suchov <asuchov42@gmail.com>
2023-04-26 16:26:47 +0200

    Testing static call

--- vars/alaraRun.groovy
+++ vars/alaraRun.groovy
@@ -1,11 +1,39 @@
-import com.falldamagestudio.Alara;
-
-void call(String cmd) {
-    Alara alara = new Alara(this);
-    alara.run(cmd);
+void call(String cmdString) {
+    runAndReturnStatus(cmdString, false);
 }
 
 void call(String cmd, Object[] arguments) {
-    Alara alara = new Alara(this);
-    alara.run(cmd, arguments);
+    String stringToCall = null;
+
+    if (arguments) {
+        String argumentsString = joinArguments(arguments);
+        stringToCall = /$cmd $argumentsString/;
+    }
+    else {
+        stringToCall = cmd;
+    }
+
+    run(stringToCall);
+}
+
+private String joinArguments(Object[] arguments) {
+    String returnString = '';
+
+    for (def argument in arguments) {
+        String stringArgument = argument;
+
+        // if argument contains any of this characters ' @><' -- add quotes around it
+        if (stringArgument.matches('.*[ @><].*')) {
+            stringArgument = /"$stringArgument"/;
+        }
+
+        // add space between arguments if needed
+        if (returnString.length() != 0) {
+            stringArgument = " $stringArgument";
+        }
+
+        returnString += stringArgument;
+    }
+
+    return returnString;
 }

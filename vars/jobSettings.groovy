1f097ed454c34db8dd42d8706456e2286f249a8d
Anton Suchov <asuchov42@gmail.com>
2023-04-27 12:09:14 +0200

    Added jobSettings function call

---
+++ vars/jobSettings.groovy
@@ -0,0 +1,13 @@
+import com.falldamagestudio.JsonHelpers
+
+Object call() {
+    String workspacePath = this.env.WORKSPACE;
+    String jobName = this.env.JOB_NAME;
+    String settingsPath = "$workspacePath/JenkinsBuildScripts/${jobName}.json";
+    if (fileExists(settingsPath)) {
+        echo("Found settings file $settingsPath");
+        return JsonHelpers.readJsonSerializable(settingsPath);
+    }
+
+    return [:];
+}

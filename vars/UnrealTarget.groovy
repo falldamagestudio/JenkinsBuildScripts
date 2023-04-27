69de2fd94755b8f090df76510cbcc870bd5b14b6
Anton Suchov <asuchov42@gmail.com>
2023-04-27 18:51:09 +0200

    Added UnrealTarget class

---
+++ vars/UnrealTarget.groovy
@@ -0,0 +1,11 @@
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class UnrealTarget {
+
+    static final String DEBUG_GAME = 'DebugGame';
+    static final String DEVELOPMENT = 'Development';
+    static final String SHIPPING = 'Shipping';
+    static final String TEST = 'Test';
+
+}
